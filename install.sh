450f8f90ada02b4dda1e7bd4dd2609915f23b918
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-05-13 18:43:35 +0700

    v0.01

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
 
+function _install() {
+local scripts="$1" dest="$2"
+
+  install ./"$scripts" /data/data/com.termux/files/home/.local/bin/"$scripts"
+  chmod +x ./"$scripts" /data/data/com.termux/files/home/.local/bin/"$scripts"
+
+}
+
+_install memorandum.sh
+_install libmemorandum-parser.py
+_install libmemorandum-unzip.py
