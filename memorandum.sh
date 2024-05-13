682ebb6b001f84b158a56980fea14c573d508701
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-05-13 18:05:43 +0700

    Create memorandum.sh

---
+++ memorandum.sh
@@ -0,0 +1,9 @@
+#!/data/data/com.termux/files/usr/bin/bash
+: "Name: MEMORANDUM (v0.01), Created by: Luis Adha (c) (2024) GNU/GPLv3"
+function main() {
+  eval "cd ~/storage/downloads"
+  eval "libmemorandum-unzip.py $(fd memo.zip | shuf -n1)"
+  eval "libmemorandum-parser.py"
+}
+
+main;
